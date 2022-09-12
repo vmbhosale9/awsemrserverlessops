11e669ee4c6e9049a6f1ddd0ce41d0e0691c1023
vmbhosale9 <vmbhosale@rediffmail.com>
2022-09-12 18:35:26 -0400

    Initial push

--- AWSEMRServerlessOps.py
+++ AWSEMRServerlessOps.py
@@ -2,15 +2,11 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 import pkg_resources
-# Testing
-
-aws_region = boto3.session.Session().region_name
-
 
 class AWSEMRServerlessOperations:
     def __init__(self) -> None:
         print("AWSEMRServerlessOperations constructor called...")
-        self.client = boto3.client("emr-serverless", region_name=aws_region)
+        self.client = boto3.client("emr-serverless", region_name=boto3.session.Session().region_name)
 
     def create_application(self, apptype, releaselabel):
         print("Create application function called...")
